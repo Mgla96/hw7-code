9e33fcac9dbb05d48dc4a43cb766915cbdd5d0b1
Diba Mirza <dimirza@cs.ucsb.edu>
2017-02-01 17:46:22 -0800

    Update stars.cpp

--- stars.cpp
+++ stars.cpp
@@ -61,7 +61,7 @@ string boxOfStars(int width, int height){
 
 string lineOfStars(int width){
     string result="";
-    for (int i=0; i<width; i++){
+    for (int j=0; j<width; j++){
         result+="*";
     }
     return result;
